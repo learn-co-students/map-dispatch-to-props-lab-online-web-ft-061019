213d37b0b4aac27a472af73d8e8e70bc2485914e
Jeremiah Rodden <jrodden1@gmail.com>
2019-10-29 16:39:36 -0600

    Done.

--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -1,16 +1,19 @@
 export default function manageRestaurants(state = {
   restaurants: []
 }, action) {
+  console.log("action", action)
+  console.log("state", state)
   switch (action.type) {
 
     case 'ADD_RESTAURANT':
+
       return {
         ...state,
         restaurants: [...state.restaurants, action.restaurant]
       }
 
     default:
-      return state;
-
+      return state;  
   }
+  
 };
